ba490a03f7275ab3608ab797f703cdd4225fb7eb
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-09-20 13:30:31 +0300

    js 2.3 fixed

--- 03-JavaScript/2.3-circle-area/script.js
+++ 03-JavaScript/2.3-circle-area/script.js
@@ -1,5 +1,6 @@
 function calculateCircleArea(radius){
-  return (Math.PI * Math.pow(radius, 2)).toFixed(2);
+  return (Math.PI * Math.pow(radius, 2));
 }
 
 console.log("Area = " +calculateCircleArea(2));
+console.log("Area = " +calculateCircleArea(2).toFixed(2));
