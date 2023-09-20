3a91453028446e38ba0f0b2cb9ab52a2306bbf48
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-09-20 13:41:02 +0300

    js 2.4

--- 03-JavaScript/2.4-date/script.js
+++ 03-JavaScript/2.4-date/script.js
@@ -1,6 +1,11 @@
-const arrayOfMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const arrayOfDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "September"];
+function getCurrentDate(){
+  const arrayOfMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const arrayOfDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "September"];
 
-const date = new Date();
+  const date = new Date();
+  // console.log(date)
 
-console.log(`Today is ${arrayOfMonths[date.getMonth()]} the ${date.getDate()} of ${arrayOfMonths[date.getMonth()]} ${date.getFullYear()}`);
+  return `Today is ${arrayOfDays[date.getDay()]} the ${date.getDate()} of ${arrayOfMonths[date.getMonth()]} ${date.getFullYear()}`;
+}
+
+console.log(getCurrentDate());
