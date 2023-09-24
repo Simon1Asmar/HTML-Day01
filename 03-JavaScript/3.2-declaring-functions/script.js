766fd7332fd657c9fe074fcd24ce8363ca650a1f
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-09-24 09:54:34 +0300

    js 3.2

--- 03-JavaScript/3.2-declaring-functions/script.js
+++ 03-JavaScript/3.2-declaring-functions/script.js
@@ -16,11 +16,34 @@ function welcome() {
   let welcome = 'Welcome to Appleseeds Bootcamp!';
   return welcome;
 }
+
+//Implicit Return
+const welcome2 = () => "Welcome to Appleseeds Bootcamp!";
+console.log(welcome2());
+
+//Explicit Return functions
 function power(a) {
   let myNumber = a;
   let result = Math.pow(myNumber, 2);
   return result;
 }
+
+//Explicit Return
+const power2 = (a) => {
+  let myNumber = a;
+  let result = Math.pow(myNumber, 2);
+  return result;
+}
+console.log(power2(3));
+
 // From function expressions to IIFE functions.
-const squareRoot = a => Math.sqrt(a);
-const randomNumbers = (a, b) => Math.random() * (a - b) + b;
+// const squareRoot = a => Math.sqrt(a);
+// const randomNumbers = (a, b) => Math.random() * (a - b) + b;
+
+(function squareRoot(a) {
+   return Math.sqrt(a);
+})();
+
+(function randomNumbers(a,b){
+  return Math.random() * (a - b) + b;
+})();
