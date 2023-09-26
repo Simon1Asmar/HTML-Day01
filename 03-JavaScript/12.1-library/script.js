b9434d68509344440b34c360cfd2fbb30d2332c6
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-09-26 16:17:51 +0300

    js 12.1

---
+++ 03-JavaScript/12.1-library/script.js
@@ -0,0 +1,27 @@
+const library = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true,
+  },
+  {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true,
+  },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
+];
+
+const readableBooks =  library.filter( b => {
+  if(b.readingStatus===true){
+    console.log(`\"${b.title}\" by ${b.author}, readable status: ${b.readingStatus}`)
+    return b.title;
+  }
+})
+
+console.log(readableBooks)
+
