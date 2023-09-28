4630f244219f942e8696ca75974ba7bd7560b3b2
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-09-28 10:17:35 +0300

    js 15.1 fixed

--- 03-JavaScript/15.1-get-sum/script.js
+++ 03-JavaScript/15.1-get-sum/script.js
@@ -1,5 +1,5 @@
 function getSum(arr1, arr2) {
-  const sum = 0;
+  let sum = 0;
   for (let i = 0; i < arr1.length; i++) {
     sum += arr1[i];
   }
@@ -7,10 +7,11 @@ function getSum(arr1, arr2) {
     sum += arr2[i];
   }
 }
-getSum([1, 2, 3][(5, 66, 23)]);
+getSum([1, 2, 3],[(5, 66, 23)]);
 
 //The bug was in line 10
 // getSum([1,2,3][5,66,23]);
+// and sum was const instead of let and then we were trying to change it
 
 /*I found it using the debugger tool in Firefox, 
 i put a breaking point next to it and started stepping forward 
@@ -18,4 +19,5 @@ but once it entered the getSum method it was still showing arr1 and arr2 as unde
 so i went back and looked at the line and saw that 
 it was not sending the correct number of arguments to the function, 
 it was missing a comma
+and also then sum wasn't changing so i switched it to let instead of const
 */
