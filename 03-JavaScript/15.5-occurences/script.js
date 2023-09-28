d12d59362c91732b1559f3bec19df63f0cbbd1e5
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-09-28 10:28:11 +0300

    js 15.5 modified

--- 03-JavaScript/15.5-occurences/script.js
+++ 03-JavaScript/15.5-occurences/script.js
@@ -2,9 +2,18 @@ function countOccurrences(str, char) {
   let counter = 0;
   for (let i = 0; i < str.length; i++) {
     if (str.charAt(i) === char) {
-      counter + 1;
+      counter += 1;
     }
   }
   return counter;
 }
 countOccurrences("ini mini miny moe", "n");
+
+//The bug was in line 5, counter + 1 wasnt actually changing the value of counter
+// counter + 1;
+
+//i put a break point next to the call to the function at line 10
+// and started stepping in using the debugger tool
+// in the scopes section the value of counter can be seen not changing
+// it stayed counter = 0
+// so the solution was to make it affect the value on the change variable by making it counter += 1;
