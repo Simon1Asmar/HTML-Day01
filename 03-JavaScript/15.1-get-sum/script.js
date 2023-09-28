f625e2421bd8a38429bcb00555c319e487578179
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-09-28 08:21:11 +0300

    js 15.1

---
+++ 03-JavaScript/15.1-get-sum/script.js
@@ -0,0 +1,21 @@
+function getSum(arr1, arr2) {
+  const sum = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    sum += arr1[i];
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    sum += arr2[i];
+  }
+}
+getSum([1, 2, 3][(5, 66, 23)]);
+
+//The bug was in line 10
+// getSum([1,2,3][5,66,23]);
+
+/*I found it using the debugger tool in Firefox, 
+i put a breaking point next to it and started stepping forward 
+but once it entered the getSum method it was still showing arr1 and arr2 as undefined 
+so i went back and looked at the line and saw that 
+it was not sending the correct number of arguments to the function, 
+it was missing a comma
+*/
