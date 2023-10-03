f57f166d8f7e3d97f40186d8084634d6e16830b7
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-10-03 22:21:56 +0300

    js 13.1

--- 03-JavaScript/13.1-sort-numbers/script.js
+++ 03-JavaScript/13.1-sort-numbers/script.js
@@ -3,6 +3,8 @@ const numbers = [1, -5, 666, 2, 400, 11];
 console.log("Ascending Order = " + numbers.sort((a,b)=>{
   return a > b;
 }));
+
+//Different way
 console.log("Decending Order = " + numbers.sort((a,b)=>{
-  return a < b;
+  return b - a;
 }));
