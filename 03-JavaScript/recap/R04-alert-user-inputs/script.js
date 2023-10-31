2fa39ab8579c2ef7bc87336b210585c7d107d481
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-10-31 13:28:46 +0200

    R04

--- 03-JavaScript/recap/R04-alert-user-inputs/script.js
+++ 03-JavaScript/recap/R04-alert-user-inputs/script.js
@@ -3,39 +3,20 @@ const form = document.querySelector("form");
 const usernameInput = document.querySelector("#username");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password")
 
 form.addEventListener("submit", formSubmitted);
 
 function formSubmitted(event){
   event.preventDefault();
-  if(usernameInput.value.length && emailInput.value.length && passwordInput.value.length){
-    //visual feedback
-    usernameInput.classList.remove("invalid-input");
-    emailInput.classList.remove("invalid-input");
-    passwordInput.classList.remove("invalid-input");
-
-    
-
-    window.alert(`Username: ${usernameInput.value}\nEmail: ${emailInput.value}\nPassword: ${passwordInput.value}`);
+  if(usernameInput.value.length && emailInput.value.length && passwordInput.value.length && confirmPassword.value.length){
+    if(passwordInput.value === confirmPassword.value){
+      window.alert(`Username: ${usernameInput.value}\nEmail: ${emailInput.value}\nPassword: ${passwordInput.value}`);
+    } else {
+      window.alert("passwords do not match");
+    }
   } else {
-    //visual feedback
-    if(!usernameInput.value.length){
-      usernameInput.classList.add("invalid-input");
-    } else {
-      usernameInput.classList.remove("invalid-input");
-    }
-
-    if(!emailInput.value.length){
-      emailInput.classList.add("invalid-input");
-    } else {
-      emailInput.classList.remove("invalid-input");
-    }
-
-    if(!passwordInput.value.length){
-      passwordInput.classList.add("invalid-input");
-    } else {
-      passwordInput.classList.remove("invalid-input");
-    }
+    window.alert("make sure all inputs are full");
   }
 
 }
