8e7d98524a458129413c3eaf9f2fb5bbef59c087
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-11-02 18:24:09 +0200

    search

--- 03-JavaScript/recap/R03-DOM-movies/script.js
+++ 03-JavaScript/recap/R03-DOM-movies/script.js
@@ -8,6 +8,63 @@ const movie = {
   img: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",
 };
 
+//DO THIS LATER (STILL NOTHING)
+const movieDatabase = {
+  movies : [
+    {
+      id: 1,
+      title: "The Shawshank Redemption",
+      genre: "Drama",
+      director: "Frank Darabont",
+      releaseYear: 1994,
+      rating: 9.3,
+      votes: 2345890,
+      img: "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg",
+    },
+    {
+      id: 2,
+      title: "The Godfather",
+      genre: "Crime, Drama",
+      director: "Francis Ford Coppola",
+      releaseYear: 1972,
+      rating: 9.2,
+      votes: 1620360,
+      img: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg",
+    },
+    {
+      id: 3,
+      title: "The Godfather: Part II",
+      genre: "Crime, Drama",
+      director: "Francis Ford Coppola",
+      releaseYear: 1974,
+      rating: 9.0,
+      votes: 1133175,
+      img: "https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    },
+    {
+      id: 4,
+      title: "The Dark Knight",
+      genre: "Action, Crime, Drama",
+      director: "Christopher Nolan",
+      releaseYear: 2008,
+      rating: 9.0,
+      votes: 2303232,
+      img: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+    },
+    {
+      id: 5,
+      title: "12 Angry Men",
+      genre: "Crime, Drama",
+      director: "Sidney Lumet",
+      releaseYear: 1957,
+      rating: 9.0,
+      votes: 689845,
+      img: "https://m.media-amazon.com/images/S/pv-target-images/b92d2865829416e35e7102a3934a2ee745f3b903a95678710442d4299d86f39c.jpg",
+    },
+  ],
+}
+//ABOVE PART STILL DOESNT DO ANYTHING
+
 const movies = [
   {
     id: 1,
@@ -65,7 +122,7 @@ const movies = [
 const body = document.body;
 const moviesList = document.querySelector("#movies-list");
 //selecting form DOM elements
-const form = document.querySelector("form");
+const form = document.querySelector("#add-movie-form");
 const titleInput = document.querySelector("#title");
 const genresInput = document.querySelector("#genres");
 const directorInput = document.querySelector("#director");
@@ -73,10 +130,19 @@ const yearInput = document.querySelector("#year");
 const ratingInput = document.querySelector("#rating");
 const votesInput = document.querySelector("#votes");
 const imgInput = document.querySelector("#img-link");
+//Seach DOM
+const searchForm = document.querySelector("#search-form");
+const searchBar = document.querySelector("#search")
 
-displayAllMovies();
+displayAllMovies(movies);
 
 form.addEventListener("submit", addNewMove);
+searchForm.addEventListener("submit", event => {
+  event.preventDefault();
+  searchMovie(searchBar.value)
+});
+
+let LAST_INDEX = movies.length - 1;
 
 function addNewMove(event){
   event.preventDefault();
@@ -95,7 +161,7 @@ function addNewMove(event){
   
     movies.push(newMovie);
   
-    displayAllMovies();
+    displayAllMovies(movies);
     // displayMovie(movies[movies.length-1]);
     form.reset();
     window.alert(`Movie id: ${newMovie.id}, "${newMovie.title} added successfully!"`)
@@ -141,10 +207,10 @@ function displayMovie(movie) {
   movieCard.append(removeButton);
 }
 
-function displayAllMovies(){
+function displayAllMovies(moviesList){
   clearMoviesList();
 
-  movies.forEach(movie => {
+  moviesList.forEach(movie => {
     displayMovie(movie);
   })
 }
@@ -162,7 +228,7 @@ function removeMovie(event){
 
   fixMovieIDs();
 
-  displayAllMovies();
+  displayAllMovies(movies);
 }
 
 function fixMovieIDs(){
@@ -170,6 +236,25 @@ function fixMovieIDs(){
     movies[i].id = i+1;
   }
 }
+
+function searchMovie(string){
+  const arrayOfMovies = [];
+
+  movies.forEach(movie => {
+    const movieTitle = movie.title.toLowerCase();
+    string = string.toLowerCase();
+    if(movieTitle.includes(string)){
+      arrayOfMovies.push(movie);
+    }
+  });
+
+  // return arrayOfMovies;
+  displayAllMovies(arrayOfMovies)
+};
+
+// make UPDATE
+
+
 
 //image link to test: zootopia
 //https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fexternal-preview.redd.it%2FwtlDFrvnyTyPUPiLYgZ8sqg57PCVyGsKeJ153K4KDoA.jpg%3Fauto%3Dwebp%26s%3D5e3b665764fa1bac64bebfd30545d143e0ac1000&f=1&nofb=1&ipt=ed95a4bbba4093a77d1ebd5df666c322fecd0405fb1c81b2690bf2a59ff38108&ipo=images
