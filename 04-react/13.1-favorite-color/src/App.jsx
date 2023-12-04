09348d27c69ae0f2b115cfa074b97145d4bc9f4c
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-12-04 14:25:59 +0200

    13.1-favcol

---
+++ 04-react/13.1-favorite-color/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+import FavColor from './components/FavColor'
+
+function App() {
+
+  return (
+    <>
+      <FavColor/>
+    </>
+  )
+}
+
+export default App
